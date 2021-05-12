99c91c83c4cca835d6c2d05d02c0cbd67ffe7ebc
Roja-Kandukuri <kandukurirojaa@gmail.com>
2021-05-12 17:07:36 +0100

    1 binaries uploaded

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -9,8 +9,18 @@ export class CoursesComponent implements OnInit {
   title = 'List of courses';
   today: number = Date.now();
   courses = [
-    { name: 'course1', description: 'mathematics', imgsrc: '' },
-    { name0: 'course2', description: 'physics', imgsrc: '' },
+    {
+      name: 'course1',
+      description: 'mathematics',
+      imgsrc:
+        'https://raw.githubusercontent.com/Roja-Kandukuri/github-pu7lcx/feature/courses-images/src/assets/images/maths.png'
+    },
+    {
+      name0: 'course2',
+      description: 'physics',
+      imgsrc:
+        'https://raw.githubusercontent.com/Roja-Kandukuri/github-pu7lcx/feature/courses-images/src/assets/images/Phisics.png'
+    },
     { name: 'course3', description: 'science', imgsrc: '' }
   ];
 
